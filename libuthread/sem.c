c9645a4e1d84156b7132e189ee4a1537315e7135
Daniel Ritchie <drritchie@ucdavis.edu>
2020-02-20 21:15:51 -0800

    cleaned up code and added 2 tests

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -8,136 +8,79 @@
 
 struct semaphore {
 	size_t count;
-        queue_t q;
+	queue_t q;
 };
-/*
- * sem_create - Create semaphore
- * @count: Semaphore count
- *
- * Allocate and initialize a semaphore of internal count @count.
- *
- * Return: Pointer to initialized semaphore. NULL in case of failure when
- * allocating the new semaphore.
- */
+
 sem_t sem_create(size_t count)
 {
-        enter_critical_section();
+	enter_critical_section();
 	sem_t new_sem = malloc(sizeof(struct semaphore));
 
-        if (new_sem == NULL) {
-                return NULL;
-        }
+	if (new_sem == NULL) {
+		return NULL;
+	}
 
-        new_sem->count = count;
-        new_sem->q = queue_create();
-        exit_critical_section();
-        return new_sem;
+	new_sem->count = count;
+	new_sem->q = queue_create();
+	exit_critical_section();
+	return new_sem;
 }
-/*
- * sem_destroy - Deallocate a semaphore
- * @sem: Semaphore to deallocate
- *
- * Deallocate semaphore @sem.
- *
- * Return: -1 if @sem is NULL or if other threads are still being blocked on
- * @sem. 0 is @sem was successfully destroyed.
- */
+
 int sem_destroy(sem_t sem)
 {
-        enter_critical_section();
+	enter_critical_section();
 	if (sem == NULL || queue_destroy(sem->q) == -1) {
-                return -1;
-        }
+		return -1;
+	}
 
-        free(sem);
-        exit_critical_section();
-        return 0;
+	free(sem);
+	exit_critical_section();
+	return 0;
 }
-/*
- * sem_down - Take a semaphore
- * @sem: Semaphore to take
- *
- * Take a resource from semaphore @sem.
- *
- * Taking an unavailable semaphore will cause the caller thread to be blocked
- * until the semaphore becomes available.
- *
- * Return: -1 if @sem is NULL. 0 if semaphore was successfully taken.
- */
+
 int sem_down(sem_t sem)
 {
-        enter_critical_section();
+	enter_critical_section();
 
-        if (sem == NULL) {
-                return -1;
-        }
-        while (sem->count < 1) {
-                pthread_t *curr_tid = malloc(sizeof(pthread_t));
-                *curr_tid = pthread_self();
-                queue_enqueue(sem->q, (void *)curr_tid);
-                //exit_critical_section();
-                thread_block();
-                enter_critical_section();
-        }
-        //enter_critical_section();
-
-        sem->count--;
-        exit_critical_section();
-        return 0;
+	if (sem == NULL) {
+		return -1;
+	}
+	while (sem->count < 1) {
+		pthread_t *curr_tid = malloc(sizeof(pthread_t));
+		*curr_tid = pthread_self();
+		queue_enqueue(sem->q, (void *)curr_tid);
+		thread_block();
+		enter_critical_section();
+	}
+	sem->count--;
+	exit_critical_section();
+	return 0;
 }
-/*
- * sem_up - Release a semaphore
- * @sem: Semaphore to release
- *
- * Release a resource to semaphore @sem.
- *
- * If the waiting list associated to @sem is not empty, releasing a resource
- * also causes the first thread (i.e. the oldest) in the waiting list to be
- * unblocked.
- *
- * Return: -1 if @sem is NULL. 0 if semaphore was successfully released.
- */
+
 int sem_up(sem_t sem)
 {
-        enter_critical_section();
-        if (sem == NULL) {
-                return -1;
-        }
+	enter_critical_section();
+	if (sem == NULL) {
+		return -1;
+	}
 
 	sem->count++;
-        //exit_critical_section();
-
-        if (queue_length(sem->q) > 0) {
-                pthread_t *tid_unblock;
-                queue_dequeue(sem->q, (void **)&tid_unblock);
-                thread_unblock(*tid_unblock);
-                free(tid_unblock);
-                //exit_critical_section();
-        }
-        exit_critical_section();
-        return 0;
+	if (queue_length(sem->q) > 0) {
+		pthread_t *tid_unblock;
+		queue_dequeue(sem->q, (void **)&tid_unblock);
+		thread_unblock(*tid_unblock);
+		free(tid_unblock);
+	}
+	exit_critical_section();
+	return 0;
 }
-/*
- * sem_getvalue - Inspect semaphore's internal state
- * @sem: Semaphore to inspect
- * @sval: Address of data item where value is received
- *
- * If semaphore @sem's internal count is greater than 0, assign internal count
- * to data item pointed by @sval.
- *
- * If semaphore @sems's internal count is equal to 0, assign a negative number
- * whose absolute value is the count of the number of threads currently blocked
- * in sem_down().
- *
- * Return: -1 if @sem or @sval are NULL. 0 if semaphore was successfully
- * inspected.
- */
+
 int sem_getvalue(sem_t sem, int *sval)
 {
 	if (sem == NULL || sval == NULL) {
-                return -1;
-        }
+		return -1;
+	}
 
-        *sval = sem->count;
-        return 0;
+	*sval = sem->count;
+	return 0;
 }
--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,359 +12,274 @@
 #include "thread.h"
 #include "tps.h"
 
-/*
- * Size of a TPS area in bytes
- */
-//#define TPS_SIZE 4096
-
 struct mempage {
-        void *memptr;
-        int num_refs;
+	void *memptr;
+	int num_refs;
 };
 
 struct tps {
-        pthread_t tid;
-        struct mempage *memarea;
+	pthread_t tid;
+	struct mempage *memarea;
 };
 
 queue_t tps_q = NULL;
 
 int find_tps(void *data, void *arg)
 {
-        if (pthread_equal(*((pthread_t *) arg), ((struct tps *) data)->tid)) {
-                return 1;
-        } else {
-                return 0;
-        }
+	if (pthread_equal(*((pthread_t *) arg), ((struct tps *) data)->tid)) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 int find_by_memarea(void *data, void *arg)
 {
-        if (((void *) arg) == ((struct tps *) data)->memarea->memptr) {
-                return 1;
-        } else {
-                return 0;
-        }       
+	if (((void *) arg) == ((struct tps *) data)->memarea->memptr) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 static void segv_handler(int sig, siginfo_t *si, __attribute__((unused)) void
-        *context)
+	*context)
 {
-        void *p_fault = (void *)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
-        
-        enter_critical_section();
-        void *temp = NULL;
-        queue_iterate(tps_q, &find_by_memarea, p_fault, &temp);
-        exit_critical_section();
-        if (temp != NULL) {
-                fprintf(stderr, "TPS protection error!\n");
-        }
+	void *p_fault = (void *)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
 
-        signal(SIGSEGV, SIG_DFL);
-        signal(SIGBUS, SIG_DFL);
+	enter_critical_section();
+	void *temp = NULL;
+	queue_iterate(tps_q, &find_by_memarea, p_fault, &temp);
+	exit_critical_section();
+	if (temp != NULL) {
+		fprintf(stderr, "TPS protection error!\n");
+	}
 
-        raise(sig);
+	signal(SIGSEGV, SIG_DFL);
+	signal(SIGBUS, SIG_DFL);
+
+	raise(sig);
 }
 
-/*
- * tps_init - Initialize TPS
- * @segv - Activate segfault handler
- *
- * Initialize TPS API. This function should only be called once by the client
- * application. If @segv is different than 0, the TPS API should install a
- * page fault handler that is able to recognize TPS protection errors and
- * display the message "TPS protection error!\n" on stderr.
- *
- * Return: -1 if TPS API has already been initialized, or in case of failure
- * during the initialization. 0 if the TPS API was successfully initialized.
- */
 int tps_init(int segv)
 {
-        enter_critical_section();
-        if (tps_q != NULL) {
-                exit_critical_section();
-                return -1;
-        }
+	enter_critical_section();
+	if (tps_q != NULL) {
+		exit_critical_section();
+		return -1;
+	}
 
-        tps_q = queue_create();
-        
-        if (tps_q == NULL) {
-                exit_critical_section();
-                return -1;
-        }
+	tps_q = queue_create();
 
-        if (segv) {
-                struct sigaction sa;
+	if (tps_q == NULL) {
+		exit_critical_section();
+		return -1;
+	}
 
-                sigemptyset(&sa.sa_mask);
-                sa.sa_flags = SA_SIGINFO;
-                sa.sa_sigaction = segv_handler;
-                sigaction(SIGBUS, &sa, NULL);
-                sigaction(SIGSEGV, &sa, NULL);
-        }
+	if (segv) {
+		struct sigaction sa;
 
-	
-        exit_critical_section();
-        return 0;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = SA_SIGINFO;
+		sa.sa_sigaction = segv_handler;
+		sigaction(SIGBUS, &sa, NULL);
+		sigaction(SIGSEGV, &sa, NULL);
+	}
+
+	exit_critical_section();
+	return 0;
 }
 
-/*
- * tps_create - Create TPS
- *
- * Create a TPS area and associate it to the current thread. The TPS area is
- * initialized to all zeros.
- *
- * Return: -1 if current thread already has a TPS, or in case of failure during
- * the creation (e.g. memory allocation). 0 if the TPS area was successfully
- * created.
- */
 int tps_create(void)
 {
-        enter_critical_section();
-        if (tps_q == NULL) {
-                return -1;
-        }
+	enter_critical_section();
+	if (tps_q == NULL) {
+		return -1;
+	}
 
 	struct tps *new_tps = malloc(sizeof(struct tps));
-        new_tps->tid = pthread_self();
-        
-        void *temp = NULL;
-        queue_iterate(tps_q, &find_tps, &(new_tps->tid), &temp);
+	new_tps->tid = pthread_self();
 
-        if (temp != NULL) {
-                free(new_tps);
-                exit_critical_section();
-                return -1;
-        }
+	void *temp = NULL;
+	queue_iterate(tps_q, &find_tps, &(new_tps->tid), &temp);
 
-        new_tps->memarea = malloc(sizeof(struct mempage));
-        if (new_tps->memarea == NULL) {
-                exit_critical_section();
-                return -1;
-        }
+	if (temp != NULL) {
+		free(new_tps);
+		exit_critical_section();
+		return -1;
+	}
 
-        new_tps->memarea->num_refs = 1;
-        new_tps->memarea->memptr = mmap(NULL, TPS_SIZE, PROT_WRITE,
-                MAP_ANON|MAP_PRIVATE, -1, 0);
-        memset(new_tps->memarea->memptr, 0, TPS_SIZE);
+	new_tps->memarea = malloc(sizeof(struct mempage));
+	if (new_tps->memarea == NULL) {
+		exit_critical_section();
+		return -1;
+	}
 
-        if (mprotect(new_tps->memarea->memptr, TPS_SIZE, PROT_NONE) < 0) {
-                exit_critical_section();
-                return -1;
-        }
+	new_tps->memarea->num_refs = 1;
+	new_tps->memarea->memptr = mmap(NULL, TPS_SIZE, PROT_WRITE,
+		MAP_ANON|MAP_PRIVATE, -1, 0);
+	memset(new_tps->memarea->memptr, 0, TPS_SIZE);
+
+	if (mprotect(new_tps->memarea->memptr, TPS_SIZE, PROT_NONE) < 0) {
+		exit_critical_section();
+		return -1;
+	}
 
 
-        queue_enqueue(tps_q, (void *)new_tps);
-        exit_critical_section();
-        return 0;
+	queue_enqueue(tps_q, (void *)new_tps);
+	exit_critical_section();
+	return 0;
 }
 
 int unmap_tps(void *data, void *arg)
 {
-        struct tps *curr_tps = data;
-        if (pthread_equal(*((pthread_t *) arg), curr_tps->tid)) {
-                mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_READ |
-                        PROT_WRITE);
-                munmap(curr_tps->memarea->memptr, TPS_SIZE);
-                return 1;
-        } else {
-                return 0;
-        }
+	struct tps *curr_tps = data;
+	if (pthread_equal(*((pthread_t *) arg), curr_tps->tid)) {
+		mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_READ |
+			PROT_WRITE);
+		munmap(curr_tps->memarea->memptr, TPS_SIZE);
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
-/*
- * tps_destroy - Destroy TPS
- *
- * Destroy the TPS area associated to the current thread.
- *
- * Return: -1 if current thread doesn't have a TPS. 0 if the TPS area was
- * successfully destroyed.
- */
 int tps_destroy(void)
 {
-        pthread_t curr_tid = pthread_self();
-	
-        enter_critical_section();
-        void *temp = NULL;
-        queue_iterate(tps_q, &unmap_tps, &curr_tid, &temp);
-        if (temp == NULL) {
-                exit_critical_section();
-                return -1;
-        } else {
-                struct tps *curr_tps = temp;
-                if (curr_tps->memarea->num_refs > 1) {
-                        curr_tps->memarea->num_refs--;
-                } else {
-                        free(((struct tps *) temp)->memarea);
-                }
+	pthread_t curr_tid = pthread_self();
 
-                queue_delete(tps_q, temp);
-                free(temp);
-                exit_critical_section();
-                return 0;
-        }
+	enter_critical_section();
+	void *temp = NULL;
+	queue_iterate(tps_q, &unmap_tps, &curr_tid, &temp);
+	if (temp == NULL) {
+		exit_critical_section();
+		return -1;
+	} else {
+		struct tps *curr_tps = temp;
+		if (curr_tps->memarea->num_refs > 1) {
+			curr_tps->memarea->num_refs--;
+		} else {
+			free(((struct tps *) temp)->memarea);
+		}
+
+		queue_delete(tps_q, temp);
+		free(temp);
+		exit_critical_section();
+		return 0;
+	}
 }
 
-/*
- * tps_read - Read from TPS
- * @offset: Offset where to read from in the TPS
- * @length: Length of the data to read
- * @buffer: Data buffer receiving the read data
- *
- * Read @length bytes of data from the current thread's TPS at byte offset
- * @offset into data buffer @buffer.
- *
- * Return: -1 if current thread doesn't have a TPS, or if the reading operation
- * is out of bound, or if @buffer is NULL, or in case of internal failure. 0 if
- * the TPS was successfully read from.
- */
 int tps_read(size_t offset, size_t length, void *buffer)
 {
 	pthread_t curr_tid = pthread_self();
 
-        enter_critical_section();
-        struct tps *curr_tps = NULL;
-        queue_iterate(tps_q, &find_tps, &curr_tid, (void **) &curr_tps);
+	enter_critical_section();
+	struct tps *curr_tps = NULL;
+	queue_iterate(tps_q, &find_tps, &curr_tid, (void **) &curr_tps);
 
-        if (curr_tps == NULL || buffer == NULL) {
-                exit_critical_section();
-                return -1;
-        }
-        if (offset + length > TPS_SIZE) {
-                exit_critical_section();
-                return -1;
-        }
+	if (curr_tps == NULL || buffer == NULL) {
+		exit_critical_section();
+		return -1;
+	}
+	if (offset + length > TPS_SIZE) {
+		exit_critical_section();
+		return -1;
+	}
 
-        if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_READ) < 0) {
-                exit_critical_section();
-                return -1;
-        }
+	if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_READ) < 0) {
+		exit_critical_section();
+		return -1;
+	}
 
-        memcpy(buffer, curr_tps->memarea->memptr + offset, length);
+	memcpy(buffer, curr_tps->memarea->memptr + offset, length);
 
-        if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_NONE) < 0) {
-                exit_critical_section();
-                return -1;
-        }
+	if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_NONE) < 0) {
+		exit_critical_section();
+		return -1;
+	}
 
-        exit_critical_section();
-        return 0;
+	exit_critical_section();
+	return 0;
 }
 
-/*
- * tps_write - Write to TPS
- * @offset: Offset where to write to in the TPS
- * @length: Length of the data to write
- * @buffer: Data buffer holding the data to be written
- *
- * Write @length bytes located in data buffer @buffer into the current thread's
- * TPS at byte offset @offset.
- *
- * If the current thread's TPS shares a memory page with another thread's TPS,
- * this should trigger a copy-on-write operation before the actual write occurs.
- *
- * Return: -1 if current thread doesn't have a TPS, or if the writing operation
- * is out of bound, or if @buffer is NULL, or in case of failure. 0 if the TPS
- * was successfully written to.
- */
 int tps_write(size_t offset, size_t length, void *buffer)
 {
 	pthread_t curr_tid = pthread_self();
 
-        enter_critical_section();
+	enter_critical_section();
 
-        struct tps *curr_tps = NULL;
-        queue_iterate(tps_q, &find_tps, &curr_tid, (void **) &curr_tps);
+	struct tps *curr_tps = NULL;
+	queue_iterate(tps_q, &find_tps, &curr_tid, (void **) &curr_tps);
 
-        if (curr_tps == NULL || buffer == NULL) {
-                exit_critical_section();
-                return -1;
-        }
-        if (offset + length > TPS_SIZE) {
-                exit_critical_section();
-                return -1;
-        }
+	if (curr_tps == NULL || buffer == NULL) {
+		exit_critical_section();
+		return -1;
+	}
+	if (offset + length > TPS_SIZE) {
+		exit_critical_section();
+		return -1;
+	}
 
-        if (curr_tps->memarea->num_refs > 1) {
-                struct mempage *newpage = malloc(sizeof(struct mempage));
-                newpage->num_refs = 1;
-                newpage->memptr = mmap(NULL, TPS_SIZE, PROT_WRITE,
-                        MAP_ANON|MAP_PRIVATE, -1, 0);
+	if (curr_tps->memarea->num_refs > 1) {
+		struct mempage *newpage = malloc(sizeof(struct mempage));
+		newpage->num_refs = 1;
+		newpage->memptr = mmap(NULL, TPS_SIZE, PROT_WRITE,
+			MAP_ANON|MAP_PRIVATE, -1, 0);
 
-                if (newpage == NULL) {
-                        free(newpage);
-                        exit_critical_section();
-                        return -1;
-                }
-                tps_read(0, TPS_SIZE, newpage->memptr);
-                curr_tps->memarea->num_refs--;
-                curr_tps->memarea = newpage;
-        } 
-        else {
-                if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_WRITE) < 0) {
-                        perror(NULL);
-                        exit_critical_section();
-                        return -1;
-                }
-        }
+		if (newpage == NULL) {
+			free(newpage);
+			exit_critical_section();
+			return -1;
+		}
+		tps_read(0, TPS_SIZE, newpage->memptr);
+		curr_tps->memarea->num_refs--;
+		curr_tps->memarea = newpage;
+	}
+	else {
+		if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_WRITE) < 0) {
+			perror(NULL);
+			exit_critical_section();
+			return -1;
+		}
+	}
 
-        memcpy(curr_tps->memarea->memptr + offset, buffer, length);
-        
-        if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_NONE) < 0) {
-                exit_critical_section();
-                return -1;
-        }
-        
-        exit_critical_section();
-        return 0;
+	memcpy(curr_tps->memarea->memptr + offset, buffer, length);
+
+	if (mprotect(curr_tps->memarea->memptr, TPS_SIZE, PROT_NONE) < 0) {
+		exit_critical_section();
+		return -1;
+	}
+
+	exit_critical_section();
+	return 0;
 }
 
-/*
- * tps_clone - Clone TPS
- * @tid: TID of the thread to clone
- *
- * Clone thread @tid's TPS. In the first phase, the cloned TPS's content should
- * copied directly. In the last phase, the new TPS should not copy the cloned
- * TPS's content but should refer to the same memory page.
- *
- * Return: -1 if thread @tid doesn't have a TPS, or if current thread already
- * has a TPS, or in case of failure. 0 is TPS was successfully cloned.
- */
 int tps_clone(pthread_t tid)
 {
-        enter_critical_section();
+	enter_critical_section();
 	struct tps *new_tps = malloc(sizeof(struct tps));
-        new_tps->tid = pthread_self();
-        
-        void *temp = NULL;
-        queue_iterate(tps_q, &find_tps, &(new_tps->tid), &temp);
-        if (temp != NULL) {
-                free(new_tps);
-                exit_critical_section();
-                return -1;
-        }
+	new_tps->tid = pthread_self();
 
-        new_tps->memarea = mmap(NULL, TPS_SIZE, PROT_WRITE,
-                MAP_ANON|MAP_PRIVATE, -1, 0);
+	void *temp = NULL;
+	queue_iterate(tps_q, &find_tps, &(new_tps->tid), &temp);
+	if (temp != NULL) {
+		free(new_tps);
+		exit_critical_section();
+		return -1;
+	}
 
-        if (new_tps->memarea == NULL) {
-                free(new_tps);
-                exit_critical_section();
-                return -1;
-        }
+	struct tps *cpy_tps = NULL;
+	queue_iterate(tps_q, &find_tps, &tid, (void **) &cpy_tps);
 
-        struct tps *cpy_tps = NULL;
-        queue_iterate(tps_q, &find_tps, &tid, (void **) &cpy_tps);
+	if (cpy_tps == NULL) {
+		exit_critical_section();
+		return -1;
+	}
 
-        if (cpy_tps == NULL) {
-                exit_critical_section();
-                return -1;
-        }
-        
-        new_tps->memarea = cpy_tps->memarea;
-        new_tps->memarea->num_refs++;
+	new_tps->memarea = cpy_tps->memarea;
+	new_tps->memarea->num_refs++;
 
-        queue_enqueue(tps_q, (void *)new_tps);
-        exit_critical_section();
-        return 0;
+	queue_enqueue(tps_q, (void *)new_tps);
+	exit_critical_section();
+	return 0;
 }
